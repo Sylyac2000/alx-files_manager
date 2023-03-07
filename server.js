6ac858d3d0f5f103e065562526c81b9ec7b39e2f
SYLLA Yacouba <sylyac2000@gmail.com>
2023-03-07 04:22:06 -0800

    2. First API upda

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import routes from './routes/index';
+import router from './routes/index';
 
 const app = express();
 const PORT = parseInt(process.env.PORT, 10) || 5000;
 
+app.use(express.json());
 // Load all routes from routes/index.js
-app.use('/', routes);
+app.use('/', router);
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 export default app;
