f92208113621bd5ac153fab618fed984618092f9
SYLLA Yacouba <sylyac2000@gmail.com>
2023-03-08 17:41:07 -0800

    2. First API nupda

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import router from './routes/index';
 
+const port = parseInt(process.env.PORT, 10) || 5000;
+
 const app = express();
-const PORT = parseInt(process.env.PORT, 10) || 5000;
 
 app.use(express.json());
-// Load all routes from routes/index.js
 app.use('/', router);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
 
 export default app;
